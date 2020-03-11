983a32d6ce1792103b2b7e80ba7f8fca8eb0e650
af9593 <moustafaallouahibi@gmail.com>
2020-03-11 18:02:51 +0100

    kill branch

--- src/client/InitClient.java
+++ src/client/InitClient.java
@@ -11,9 +11,8 @@ public class InitClient {
 		//use this instance if you want to connect to the server on the Virtual Machine. If the VM is off use localhost
 		// ClientCommunications clientCommunications = new ClientCommunications("3.132.184.132", 5555, data);
 
-
 		//use this instance if you want to connect to localhost.Do not forget to start the server first on your computer (InitServer)
-		ClientCommunications clientCommunications = new ClientCommunications("127.0.0.1", 5555, data);
+		ClientCommunications clientCommunications = new ClientCommunications("localhost", 5555, data);
 
 		LogInController logInController = new LogInController(clientCommunications, "Test1", "password");
 
